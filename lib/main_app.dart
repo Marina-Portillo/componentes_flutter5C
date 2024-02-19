4f2cf1a19f765a35de3c1ddb11efa9a679fc5bdf
Marina <maryypucca@gmail.com>
2024-02-19 09:46:22 -0600

    Add Theme

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:practica3/home_screen.dart';
 
+
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: const HomeScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 60, 183, 240),
+        appBarTheme: const AppBarTheme(color: Color.fromARGB(235, 25, 48, 247),),
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+            color: Color.fromARGB(255, 5, 7, 4),
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'sans-serif',
+          )
+        ),
+      ),
     );
   }
 }
