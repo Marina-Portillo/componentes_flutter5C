39c96c53f123bf82f40624234daaa05f9b1cff6d
Marina <maryypucca@gmail.com>
2024-03-06 14:00:08 -0600

    Images 2

--- lib/screens/images_screen.dart
+++ lib/screens/images_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class imagesScreen extends StatefulWidget {
   const imagesScreen({super.key});
 
@@ -19,6 +20,7 @@ class _imagesScreenState extends State<imagesScreen> {
       body: ListView(
         children: [
           imageCard(),
+          imageWeb(),
         ],
       ) );
   }
@@ -26,10 +28,31 @@ class _imagesScreenState extends State<imagesScreen> {
     return Card(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(30)),
-        child: const Image(
-            image: AssetImage('assets/imgs/imagen3.gif'),
-              ),
-        
+        margin: const EdgeInsets.all(20),
+        elevation: 10,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: Column(
+              children: [
+                const Image(
+              image: AssetImage('assets/imgs/imagen3.gif'),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: const Text('Sasuke Uchiha' ,
+                        style: TextStyle(fontSize: 30)
+                        ),
+                ),
+              ],
+          ),
+        ), 
+    );
+  }
+  Widget imageWeb(){
+    return Center(
+      child: Image.network(
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsoFIilEBtqfLNWi9xrtFqTcQY4vp7IWHKsA&usqp=CAU'
+        ),
     );
   }
 }
